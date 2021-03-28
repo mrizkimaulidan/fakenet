d03b581657385fcdfe8847e22dd7a7eda6a7666b
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-03-28 16:15:22 +0800

    alert message

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -5,6 +5,7 @@
 @section('content')
 <div class="row">
     <div class="col-md-12">
+        @include('components.alert-message')
         <div class="card shadow mb-4">
             <div class="card-header py-3">
                 <h6 class="m-0 font-weight-bold text-primary">Data Transaksi</h6>
