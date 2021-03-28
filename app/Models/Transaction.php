6ab684f2c68b4fd7b6d038b33ea884f8d89d7e49
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-03-28 13:37:06 +0800

    remove function on transaction model

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -10,9 +10,4 @@ class Transaction extends Model
     use HasFactory;
 
     protected $fillable = ['client_id', 'user_id', 'day', 'month', 'amount', 'is_paid', 'date'];
-
-    public function is_paid_status(int $is_paid)
-    {
-        return $is_paid === 1 ? 'Lunas' : 'Belum Lunas';
-    }
 }
