30f3b11a6944dd2cf4dc3e8061ea76f4758bfd71
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-03-31 20:12:44 +0800

    redirect when accessing root path

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
 });
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth'])->name('dashboard');
