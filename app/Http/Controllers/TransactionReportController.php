5303cb392ee702375365e8ab5a93f0ddbeaaff28
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-04-03 14:37:34 +0800

    report

--- app/Http/Controllers/TransactionReportController.php
+++ app/Http/Controllers/TransactionReportController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
 use App\Repositories\TransactionReportRepository;
 use Illuminate\Http\Request;
 
@@ -17,77 +18,15 @@ class TransactionReportController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        if ($request->get('month') !== null) {
+            $transactions = Transaction::where('month', $request->get('month'))->where('year', $request->get('year'))->get();
+        }
+
         $transactions_this_month = $this->transactionReportRepository->getTransactionsBy(month: date('m'));
         $transactions_this_year = $this->transactionReportRepository->getTransactionsBy(year: date('Y'));
 
         return view('reports.transactions.index', compact('transactions_this_month', 'transactions_this_year'));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
--- resources/views/reports/transactions/index.blade.php
+++ resources/views/reports/transactions/index.blade.php
@@ -5,11 +5,34 @@
 @section('content')
 <div class="row">
     <div class="col-md-12">
+        <div class="card py-2 px-2">
+            <form action="" method="GET">
+                <label for="">Filter transaksi berdasarkan bulan dan tahun</label>
+                <div class="input-group">
+                    <select name="month" id="month" class="form-control">
+                        <option selected>Pilih bulan..</option>
+                        @foreach (range(1, 31) as $day)
+                        <option value="{{ sprintf('%02d', $day) }}">{{ sprintf('%02d', $day) }}</option>
+                        @endforeach
+                    </select>
+
+                    <input type="number" name="year" id="year" class="form-control" placeholder="Masukkan tahun">
+
+                    <button type="submit" class="btn btn-primary mx-1">Filter</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<div class="row my-2">
+    <div class="col-md-12">
         <div class="card shadow mb-4">
             <div class="card-header py-3">
-                <h6 class="m-0 font-weight-bold text-primary">Data Transaksi Bulan Ini</h6>
+                <h6 class="m-0 font-weight-bold text-primary">Data Laporan Tagihan</h6>
             </div>
             <div class="card-body">
+                <button class="btn btn-primary float-right mb-3">Export Excel</button>
                 <div class="table-responsive">
                     <table class="table table-bordered" id="datatable" width="100%" cellspacing="0">
                         <thead class="text-center">
@@ -17,22 +40,16 @@
                                 <th>#</th>
                                 <th>Nama Klien</th>
                                 <th>Tanggal</th>
-                                <th>Total Bayar</th>
                                 <th>Penagih</th>
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($transactions_this_month as $transaction_this_month)
                             <tr>
-                                <td>{{ $loop->iteration }}</td>
-                                <td>{{ $transaction_this_month->client->name }}</td>
-                                <td>
-                                    {{ $transaction_this_month->day }}-{{ $transaction_this_month->month }}-{{ $transaction_this_month->year }}
-                                </td>
-                                <td>{{ indonesian_currency($transaction_this_month->amount) }}</td>
-                                <td>{{ $transaction_this_month->user->name }}</td>
+                                <td>1</td>
+                                <td>1</td>
+                                <td>1</td>
+                                <td>1</td>
                             </tr>
-                            @endforeach
                         </tbody>
                     </table>
                 </div>
