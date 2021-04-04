88724e41e624331f7cc1d201e5c4c0044bed0e79
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-04-04 15:49:46 +0800

    moving

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,6 +22,10 @@
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
 
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 class="h3 mb-0 text-gray-800">{{ $title }}</h1>
+                    </div>
+
                     <!-- Content Row -->
                     @yield('content')
 
