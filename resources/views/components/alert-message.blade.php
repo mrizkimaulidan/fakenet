db1418c179e8d5fce69060584d46dd3874880c1e
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-04-05 14:07:14 +0800

    added more flash message session

--- resources/views/components/alert-message.blade.php
+++ resources/views/components/alert-message.blade.php
@@ -6,3 +6,22 @@
     </button>
 </div>
 @endif
+
+@if(request()->session()->get('warning'))
+<div class="alert alert-warning alert-dismissible fade show" role="alert">
+    <strong>Peringatan!</strong> {{ session('warning') }}.
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+@endif
+
+@if(count($errors) !== 0)
+<div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <strong>Gagal!</strong>
+    <ul>
+        @foreach($errors->all() as $message)
+        <li>{{ $message }}</li>
+        @endforeach
+    </ul>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+@endif
