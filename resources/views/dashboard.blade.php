7c6524296735e3a1a84614f686500687bda02051
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-04-08 13:16:09 +0800

    added year

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -109,7 +109,7 @@
         <div class="card shadow mb-4">
             <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                 <h6 class="m-0 font-weight-bold text-primary">
-                    Statistik Transaksi Tahun Ini
+                    Statistik Transaksi Tahun Ini ({{ date('Y') }})
                 </h6>
             </div>
             <div class="card-body">
