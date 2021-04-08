f0975daf5532ee7f05104eb393a91aa9f6c7a778
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-04-08 12:57:39 +0800

    styling validation error on each input

--- resources/views/administrator_applications/modal/create.blade.php
+++ resources/views/administrator_applications/modal/create.blade.php
@@ -14,49 +14,69 @@
                     @csrf
                     <div class="form-group">
                         <label for="name">Nama</label>
-                        <input type="text" class="form-control" name="name" id="name" value="{{ old('name') }}"
-                            placeholder="Masukkan nama">
+                        <input type="text" class="form-control @error('name') is-invalid @enderror" name="name"
+                            id="name" value="{{ old('name') }}" placeholder="Masukkan nama">
+                        @error('name')
+                        <small class="d-block font-weight-bold invalid-feedback">{{ $errors->first('name') }}</small>
+                        @enderror
                     </div>
 
                     <div class="form-group">
                         <label for="position_id">Jabatan</label>
-                        <select name="position_id" class="form-control" id="position_id">
+                        <select name="position_id" class="form-control @error('position_id') is-invalid @enderror"
+                            id="position_id">
                             <option selected>Pilih..</option>
                             @foreach ($positions as $position)
                             <option value="{{ $position->id }}">{{ $position->name }}</option>
                             @endforeach
                         </select>
+                        @error('position_id')
+                        <small
+                            class="d-block font-weight-bold invalid-feedback">{{ $errors->first('position_id') }}</small>
+                        @enderror
                     </div>
 
                     <div class="form-group">
                         <label for="email">Alamat Email</label>
-                        <input type="email" class="form-control" name="email" id="email" value="{{ old('email') }}"
-                            placeholder="Masukkan alamat email">
+                        <input type="email" class="form-control @error('email') is-invalid @enderror" name="email"
+                            id="email" value="{{ old('email') }}" placeholder="Masukkan alamat email">
+                        @error('email')
+                        <small class="d-block font-weight-bold invalid-feedback">{{ $errors->first('email') }}</small>
+                        @enderror
                     </div>
 
                     <div class="form-group">
                         <label for="password">Password</label>
                         <div class="input-group">
-                            <input type="password" class="form-control" name="password" id="password"
-                                placeholder="Masukkan password">
+                            <input type="password" class="form-control @error('password') is-invalid @enderror"
+                                name="password" id="password" placeholder="Masukkan password">
                             <div class="input-group-append">
                                 <button class="btn btn-outline-secondary" type="button" id="password-button-eye"><i
                                         class="fas fa-eye" id="password-eye-class"></i></button>
                             </div>
                         </div>
+                        @error('password')
+                        <small
+                            class="d-block font-weight-bold invalid-feedback">{{ $errors->first('password') }}</small>
+                        @enderror
                     </div>
 
                     <div class="form-group">
                         <label for="password_confirmation">Ulangi Password</label>
                         <div class="input-group">
-                            <input type="password" class="form-control" name="password_confirmation"
-                                id="password_confirmation" placeholder="Ulangi password">
+                            <input type="password"
+                                class="form-control @error('password_confirmation') is-invalid @enderror"
+                                name="password_confirmation" id="password_confirmation" placeholder="Ulangi password">
                             <div class="input-group-append">
                                 <button class="btn btn-outline-secondary" type="button"
                                     id="password-confirmation-button-eye"><i class="fas fa-eye"
                                         id="password-confirmation-eye-class"></i></button>
                             </div>
                         </div>
+                        @error('password_confirmation')
+                        <small
+                            class="d-block font-weight-bold invalid-feedback">{{ $errors->first('password_confirmation') }}</small>
+                        @enderror
                     </div>
             </div>
             <div class="modal-footer">
