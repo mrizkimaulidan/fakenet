430bbb34f4304ffb4d9ec8388417a424953e1808
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-04-18 13:09:26 +0800

    remove auth middleware

--- routes/api.php
+++ routes/api.php
@@ -21,13 +21,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::middleware('auth')->group(function () {
-    Route::get('/chart-monthly', [DashboardChartController::class, 'chartMonthly'])->name('api.chart.monthly');
 
-    Route::get('/tagihan/detail/{id}', [TransactionController::class, 'detail'])->name('api.tagihan.detail');
-    Route::get('/tagihan/{id}', [TransactionController::class, 'show'])->name('api.tagihan.show');
-    Route::get('/tagihan/detail/klien/{id}', [TransactionController::class, 'clientDetail'])->name('api.tagihan-client.detail');
+Route::get('/chart-monthly', [DashboardChartController::class, 'chartMonthly'])->name('api.chart.monthly');
 
-    Route::get('/paket-internet/{id}', [InternetPackageController::class, 'show'])->name('api.paket-internet.show');
-    Route::get('/administrator-aplikasi/{id}', [AdministratorApplicationController::class, 'show'])->name('api.administrator-aplikasi.show');
-});
+Route::get('/tagihan/detail/{id}', [TransactionController::class, 'detail'])->name('api.tagihan.detail');
+Route::get('/tagihan/{id}', [TransactionController::class, 'show'])->name('api.tagihan.show');
+Route::get('/tagihan/detail/klien/{id}', [TransactionController::class, 'clientDetail'])->name('api.tagihan-client.detail');
+
+Route::get('/paket-internet/{id}', [InternetPackageController::class, 'show'])->name('api.paket-internet.show');
+Route::get('/administrator-aplikasi/{id}', [AdministratorApplicationController::class, 'show'])->name('api.administrator-aplikasi.show');
